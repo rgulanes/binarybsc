734d2041ddfc4f3b24991e7de52c4f7a5d09f2db
Ronald Jay Gulanes <rgulanes@users.noreply.github.com>
2017-01-29 23:29:38 +0800

    Galleries
    * FIX - Issues on deleted album still being shown on Galleries.

--- database/Stored Procedure/get_GalleryAlbums.sql
+++ database/Stored Procedure/get_GalleryAlbums.sql
@@ -8,6 +8,6 @@ BEGIN
 		tr.node_desc AS parent , t.node_desc AS child FROM photo_tbl p
 		LEFT JOIN tree_node t ON t.node_id = p.album_id
 		JOIN tree_node tr ON tr.node_id = t.parent_node
-	WHERE is_deleted <> 1 AND img_status = 1 GROUP BY p.album_id;
+	WHERE ((tr.node_status != 2) AND (t.node_status != 2)) AND is_deleted <> 1 AND img_status = 1 GROUP BY p.album_id;
 END$$
 DELIMITER ;
